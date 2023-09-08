fd1b231c1790e044c44cef4593b50606656aa6a5
Sidney Basa <basa206@hotmail.com>
2023-09-07 22:26:22 -0700

    delete button for each seller added

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -49,6 +49,14 @@ const Sellers = () => {
     //         setIsLoading(false);
     //         setErrors(error.message)}
     //     }
+    const deleteSeller = id => {
+        setSellers(sellers.filter(s => s.id !== id))
+        axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .catch(error => {
+            setErrors(error.message)
+            setSellers(sellers)
+        })
+    }
 
     return (
     <>
@@ -57,7 +65,18 @@ const Sellers = () => {
         <button onClick={addSeller}>Add Seller</button>
         {isLoading && <Loader />}
         {errors && <em>{errors}</em>}
-        {sellers.map(sellers => <p key={sellers.id}>{sellers.name}</p>)}
+
+        <table>
+            <tbody>
+                    {sellers.map(seller => 
+                <tr key={seller.id}>
+                    <td>{seller.name}</td>
+                    <td><button onClick={() => deleteSeller(seller.id)}>Delete</button></td>
+                </tr>
+                )}
+            </tbody>
+        </table>
+        
     </>
     )
 };
