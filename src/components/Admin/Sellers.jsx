c7b2d2acbc9793393efc0875eaa526f7360a8c1b
Sidney Basa <basa206@hotmail.com>
2023-09-07 22:11:12 -0700

    adding a new seller to Admin panel

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -10,35 +10,51 @@ const Sellers = () => {
     const [errors, setErrors] = useState("")
 
     useEffect(() => {
-        fetchSellers()
-        // setIsLoading(true)
-        // axios.get("https://jsonplaceholder.typicode.com/users")
-        // .then(response => { 
-        //     setSellers(response.data)
-        //     setIsLoading(false)
-        //     }
-        //     ).catch(error => {
-        //         setIsLoading(false);
-        //         setErrors(error.message)}
-        //         )
-    }, []);
-
-    const fetchSellers = async() => {
+        // fetchSellers()
         setIsLoading(true)
-        try {
-            const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(response => { 
             setSellers(response.data)
             setIsLoading(false)
-        } catch (error) {
-            setIsLoading(false);
-            setErrors(error.message)}
+            }
+            ).catch(error => {
+                setIsLoading(false);
+                setErrors(error.message)}
+                )
+    }, []);
+
+    const addSeller = () => {
+        const newSeller = {
+            // is the same as the key value pair:
+            // name: name 
+            name,
+            id: sellers.length + 1, 
         }
-    
+        setSellers([newSeller, ...sellers]);
+        axios.post("https://jsonplaceholder.typicode.com/users",
+        newSeller).then(response => setSellers([response.data, ...sellers]))
+        .catch(error => {
+            setErrors(error.message)
+            setSellers(sellers)
+        })
+    }
+
+    // const fetchSellers = async() => {
+    //     setIsLoading(true)
+    //     try {
+    //         const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+    //         setSellers(response.data)
+    //         setIsLoading(false)
+    //     } catch (error) {
+    //         setIsLoading(false);
+    //         setErrors(error.message)}
+    //     }
 
     return (
     <>
         <h3>Admin Sellers Page</h3>;
         <input type="text" onChange={e => setName(e.target.value)} />
+        <button onClick={addSeller}>Add Seller</button>
         {isLoading && <Loader />}
         {errors && <em>{errors}</em>}
         {sellers.map(sellers => <p key={sellers.id}>{sellers.name}</p>)}
